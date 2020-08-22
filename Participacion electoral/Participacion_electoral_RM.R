35027da1e37009a5181ce010b6d1dddd58df1e59
jose-pino18 <jose.pino.jauregui@gmail.com>
2020-08-22 12:12:04 +0200

    Terminamos la limipieza de los datos y creamos un archivo que contiene la participacion electoral de cada comuna de la RM desde 2012

--- Participacion electoral/Participacion_electoral_RM.R
+++ Participacion electoral/Participacion_electoral_RM.R
@@ -31,6 +31,13 @@ votacion<- votacion %>%
 #Calculamos la participacion electoral a nivel comunal en la RM
 votacion<- votacion %>%
   mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Pres_2013")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Pres_2013<-round(votacion$Pres_2013,2)
+#Seleccionamos nuestras variables a ocupar
+Presidenciales_2013<-votacion %>%
+  select(Comuna, Pres_2013)
 #Pasamos nuestro valor de participación a un numero con dos decimanles.
 ##### Elecciones municipales 2012 ####
 #Cargamos la base de datos a ocupar
@@ -62,6 +69,184 @@ votacion<- votacion %>%
 #Calculamos la participacion electoral a nivel comunal en la RM
 votacion<- votacion %>%
   mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Mun_2012")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Mun_2012<-round(votacion$Mun_2012,2)
+#Seleccionamos nuestras variables a ocupar
+Municipales_2012<-votacion %>%
+  select(Comuna, Mun_2012)
 
 ##### Elecciones 2013 segunda vuelta ####
-#Carfamos el dataset
+#Cargamos el dataset
+Elecciones_2013_2V <- read_delim("E:/Documentos/Trabajo/electoral/participacion/e_2013_2V/VW_VOTARON_2013_2V.csv", 
+                               ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                               trim_ws = TRUE) #Este archivo tenía una codificación distinta
+
+Elecciones_2013_2V$SEXO<-as.factor(Elecciones_2013_2V$SEXO)
+Elecciones_2013_2V$SUFRAGIO<-as.factor(Elecciones_2013_2V$SUFRAGIO)
+Elecciones_2013_2V$COMUNA<-as.factor(Elecciones_2013_2V$COMUNA)
+#Filtraremos nuestra base de datos, queremos las comunas de la RM y la gente que participo de ella (un resumen)
+participacion<-Elecciones_2013_2V %>%
+  group_by(COMUNA) %>% 
+  filter(REGION=="Metropolitana De Santiago")%>%
+  count(SUFRAGIO)
+
+## creamos columnas independientes con la gente que voto y la que se abstuvo
+voto<- participacion %>%
+  filter(SUFRAGIO=="sufragó")
+
+no_voto<-participacion %>%
+  filter(SUFRAGIO=="no sufragó")
+## Unimos ambas columnas a un nuevo DF creado, llamado votación
+votacion<- voto%>%
+  left_join(no_voto, by= "COMUNA")
+##Calulamos el total del padron (suma de votantes como no votantes)
+votacion<- votacion %>%
+  mutate(padron= sum(n.x + n.y))
+#Calculamos la participacion electoral a nivel comunal en la RM
+votacion<- votacion %>%
+  mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Pres_2013_2V")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Pres_2013_2V<-round(votacion$Pres_2013_2V,2)
+#Seleccionamos nuestras variables a ocupar
+Presidenciales_2013_2V<-votacion %>%
+  select(Comuna, Pres_2013_2V)
+
+##### Elecciones Municipales 2016 ####
+Muni_2016 <- read_delim("E:/Documentos/Trabajo/electoral/participacion/e_2016/PARTICIPACION_2016.csv", 
+                        ";", escape_double = FALSE, trim_ws = TRUE) #Este archivo tenía una codificación tradicional
+
+Muni_2016$SEXO<-as.factor(Muni_2016$SEXO)
+Muni_2016$SUFRAGIO<-as.factor(Muni_2016$SUFRAGIO)
+Muni_2016$COMUNA<-as.factor(Muni_2016$COMUNA)
+#Filtraremos nuestra base de datos, queremos las comunas de la RM y la gente que participo de ella (un resumen)
+participacion<-Muni_2016 %>%
+  group_by(COMUNA) %>% 
+  filter(REGION=="Metropolitana De Santiago")%>%
+  count(SUFRAGIO)
+
+## creamos columnas independientes con la gente que voto y la que se abstuvo
+voto<- participacion %>%
+  filter(SUFRAGIO=="sufragó")
+
+no_voto<-participacion %>%
+  filter(SUFRAGIO=="no sufragó")
+## Unimos ambas columnas a un nuevo DF creado, llamado votación
+votacion<- voto%>%
+  left_join(no_voto, by= "COMUNA")
+##Calulamos el total del padron (suma de votantes como no votantes)
+votacion<- votacion %>%
+  mutate(padron= sum(n.x + n.y))
+#Calculamos la participacion electoral a nivel comunal en la RM
+votacion<- votacion %>%
+  mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Mun_2016")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Mun_2016<-round(votacion$Mun_2016,2)
+#Seleccionamos nuestras variables a ocupar
+Municipales_2016<-votacion %>%
+  select(Comuna, Mun_2016)
+
+##### Elecciones 2017 (1V y Parlamentarias)####
+Elecciones_2017_1V <- read_delim("E:/Documentos/Trabajo/electoral/participacion/e_2017/VW_VOTARON_2017_1V.csv", 
+                                 ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                                 trim_ws = TRUE) 
+##Convertimos a factor 
+Elecciones_2017_1V$SEXO<-as.factor(Elecciones_2017_1V$SEXO)
+Elecciones_2017_1V$SUFRAGIO<-as.factor(Elecciones_2017_1V$SUFRAGIO)
+Elecciones_2017_1V$COMUNA<-as.factor(Elecciones_2017_1V$COMUNA)
+#Filtraremos nuestra base de datos, queremos las comunas de la RM y la gente que participo de ella (un resumen)
+participacion<-Elecciones_2017_1V %>%
+  group_by(COMUNA) %>% 
+  filter(REGION=="Metropolitana De Santiago")%>%
+  count(SUFRAGIO)
+
+## creamos columnas independientes con la gente que voto y la que se abstuvo
+voto<- participacion %>%
+  filter(SUFRAGIO=="sufragó")
+
+no_voto<-participacion %>%
+  filter(SUFRAGIO=="no sufragó")
+## Unimos ambas columnas a un nuevo DF creado, llamado votación
+votacion<- voto%>%
+  left_join(no_voto, by= "COMUNA")
+##Calulamos el total del padron (suma de votantes como no votantes)
+votacion<- votacion %>%
+  mutate(padron= sum(n.x + n.y))
+#Calculamos la participacion electoral a nivel comunal en la RM
+votacion<- votacion %>%
+  mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Pres_2017")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Pres_2017<-round(votacion$Pres_2017,2)
+#Seleccionamos nuestras variables a ocupar
+Presidenciales_2017<-votacion %>%
+  select(Comuna, Pres_2017)
+
+##### Elecciones 2017 segunda vuelta ####
+#Cargamos datos
+Elecciones_2017_2V <- read_delim("E:/Documentos/Trabajo/electoral/participacion/e_2017_2V/VW_VOTARON_2017_2V.csv", 
+                                 ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
+                                 trim_ws = TRUE) 
+##Convertimos a factor 
+Elecciones_2017_2V$SEXO<-as.factor(Elecciones_2017_2V$SEXO)
+Elecciones_2017_2V$SUFRAGIO<-as.factor(Elecciones_2017_2V$SUFRAGIO)
+Elecciones_2017_2V$COMUNA<-as.factor(Elecciones_2017_2V$COMUNA)
+#Filtraremos nuestra base de datos, queremos las comunas de la RM y la gente que participo de ella (un resumen)
+participacion<-Elecciones_2017_2V %>%
+  group_by(COMUNA) %>% 
+  filter(REGION=="Metropolitana De Santiago")%>%
+  count(SUFRAGIO)
+
+## creamos columnas independientes con la gente que voto y la que se abstuvo
+voto<- participacion %>%
+  filter(SUFRAGIO=="sufragó")
+
+no_voto<-participacion %>%
+  filter(SUFRAGIO=="no sufragó")
+## Unimos ambas columnas a un nuevo DF creado, llamado votación
+votacion<- voto%>%
+  left_join(no_voto, by= "COMUNA")
+##Calulamos el total del padron (suma de votantes como no votantes)
+votacion<- votacion %>%
+  mutate(padron= sum(n.x + n.y))
+#Calculamos la participacion electoral a nivel comunal en la RM
+votacion<- votacion %>%
+  mutate(parti= n.x*100/ padron)
+#Renombramos nuestras variables
+names (votacion) = c("Comuna", "Votó", "Cantidad", "Abstuvo", "Cantidad", "Padron", "Pres_2017_2V")
+#Redondeamos los valores obtenidos de particiacion
+votacion$Pres_2017_2V<-round(votacion$Pres_2017_2V,2)
+#Seleccionamos nuestras variables a ocupar
+Presidenciales_2017_2V<-votacion %>%
+  select(Comuna, Pres_2017_2V)
+
+##### Posible Mejoras ####
+#Hacer una función o loop para no repetir tanto el proceso por cada una de las elecciones
+
+
+####  unir datasets ####
+participacion_RM<- Presidenciales_2013 %>%
+  left_join(Municipales_2012, by= "Comuna")
+
+participacion_RM<- participacion_RM %>%
+  left_join(Presidenciales_2013_2V, by= "Comuna")
+
+participacion_RM<- participacion_RM %>%
+  left_join(Municipales_2016, by= "Comuna")
+
+participacion_RM<- participacion_RM %>%
+  left_join(Presidenciales_2017, by= "Comuna")
+
+participacion_RM<- participacion_RM %>%
+  left_join(Presidenciales_2017_2V, by= "Comuna")
+
+participacion_RM<-participacion_RM %>%
+  select("Comuna", "Mun_2012", "Pres_2013", "Pres_2013_2V","Mun_2016", "Pres_2017", "Pres_2017_2V")
+#Guardar nuestra base de datos como archivo csv
+write.csv(participacion_RM,"Participacion_electoral_RM.csv")
